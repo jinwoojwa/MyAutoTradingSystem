9fa9471a9c2ef241d195fca7cb7c39ac42398392
jinwoojwa <jjw6654@gmail.com>
2026-02-05 13:57:07 +0900

    init: project setup

---
+++ src/main/java/com/myproject/hellomytrader/HelloMyTraderApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.hellomytrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMyTraderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMyTraderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myproject/hellomytrader/HelloMyTraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.hellomytrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMyTraderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
